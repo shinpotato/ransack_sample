4a9b66c5623cddb2c70c031856b3ee472d4dec9e
shinpotato <potatoandtomato2@gmail.com>
2020-01-26 11:19:24 +0900

    second_fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
 
   def search
-    user_search = UserSearch.new(params_user_search)
+    user_search = UserSearch.new(search_name: params[:search_name],search_age: params[:search_age])
+    #user_search = UserSearch.new(params_user_search)
     @users = user_search.execute
   end
 
